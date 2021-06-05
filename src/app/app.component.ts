5b419f6a66479265963943b3cdad1e466a2372fd
maikofelix47 <maikofelix89@gmail.com>
2021-06-05 14:42:20 +0300

    Fixed calculation issues and added number pipe for 2 decimal places

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent implements OnInit{
    public tc: number = 0;
    public it: number = 555;
    public lo: number = 1367;
-   public tempDiff: number = Number((2 * this.dP * (this.tb - this.tc)*(this.it/this.lo)).toFixed(1));
+   public tempDiff: number = Number(2 * this.dP * (this.tb - this.tc)*(this.it/this.lo));
   
 
    //section 2
@@ -25,36 +25,39 @@ export class AppComponent implements OnInit{
    public mw: number = 0;
 
   //section 3
-   public va = 0;
+
    public taLK = 304.06;
-   public atD:number = 27;
+   public atD:number = 0;
    public atK: number = this.atD + 273;
    public taLD:number = this.atD + (this.tempDiff);
    public taLHD: number = 43.2;
    public to: number = this.taLD + 273;
-   public pa = 101800.0;
-   public cpa = 1006.0;
+   public pa:number = 101800.0;
+   public cpa:number = 1006.0;
 
    //section 3.10
    public ra = 286.9;
    public rg: number = 461.5;
    public tb2: number = 373;
    public pc: number = 22060000;
-   public tp: number = Number((0.25 * ((3 * this.taLD) + this.atD)).toFixed(2));
+   public tp: number = Number(0.25 * ((3 * this.taLD) + this.atD));
    public tpt: number = this.tp + 273;
    public vtc: number = 650;
    public lt: number = Math.round(Number(this.rg * this.vtc*this.tb2)*(Math.log(this.pc/Math.pow(10,5)))*(Math.pow((this.vtc-this.tpt),0.38))/(Math.pow((this.vtc-this.tb2),1.38)));
 
+   //section3 dependant on 3.10
+   public va = (this.mw * this.lt * this.ra * this.atD)/(this.cpa * this.pa*(this.to - this.taLK));
+   
    //section 4
-   public v: number = 0.993831219245376;
-   public mfr: number = 1.19259746309445;
-   public vam = '';
    public t: number = 86400;
+   public v: number = Number((this.va / this.t));
+   public vam = '';
    public p:number = 1.2;
+   public mfr: number = Number(this.v /this.p);
 
    //section 5
    public n: number =  0.125;
-   public smw: number = 529.41;
+   public smw: number = 0;
    public slt: number = 2.35E+06;
    public sit: number = 555;
    public st: number = 86400;
@@ -84,12 +87,18 @@ export class AppComponent implements OnInit{
 
    public onWwChange($event){
     this.calculateMm();
+    //this.calculateSolarEnergyCollectionArea();
     this.calculateMasOfCassavaToDry();
     this.calculateLoadingDensity();
+    this.calculateVolumeOfAirRequiredToRemoveMositure();
+    this.calculateVolumeFlowRate();
+    this.calculateMassFlowRate();
+    this.calculateNumberOfChimneys();
    }
 
    public calculateMm(){
-    this.mw = Number((this.ww * ((this.miwb/100) - (this.mfwb)/100)/(1 - (this.mfwb)/100)).toFixed(2));
+    this.mw = Number(this.ww * ((this.miwb/100) - (this.mfwb)/100)/(1 - (this.mfwb)/100));
+    this.calculateSolarEnergyCollectionArea();
    }
    public onMiwbChange($event){
     this.calculateMm();
@@ -104,7 +113,7 @@ export class AppComponent implements OnInit{
     this.calculateSolarEnergyCollectionArea();
    }
    public calculateSolarEnergyCollectionArea(){
-     this.ca = Math.round((this.smw * this.slt)/(this.sit * this.st * this.n));
+     this.ca = Math.round((this.mw * this.slt)/(this.sit * this.st * this.n));
    }
    public calculateLoadingDensity(){
       this.ld = (this.dww/this.sa);
@@ -115,13 +124,17 @@ export class AppComponent implements OnInit{
     }
    }
    public calculateNumberOfChimneys(){
-      this.nc = Math.round(this.cv/(this.w * this.s * this.vc));
+      this.nc = Math.round(this.v/(this.w * this.s * this.vc));
    }
    public onAtDChange($event){
      this.calculateAmbientTempInKelvin();
      this.calculateTempOfAirLeavingHeaterInDegrees();
      this.calculateTemparatureOfProduct();
      this.calculateEnthalpyOfVaporization();
+     this.calculateVolumeOfAirRequiredToRemoveMositure();
+     this.calculateVolumeFlowRate();
+     this.calculateMassFlowRate();
+     this.calculateNumberOfChimneys();
    }
    public calculateAmbientTempInKelvin(){
       this.atK = Number(this.atD) + 273;
@@ -134,7 +147,7 @@ export class AppComponent implements OnInit{
     this.to = this.taLD + 273;
    }
    public calculateTemparatureOfProduct(){
-    this.tp = Number((0.25 * ((3 * this.taLD) + Number(this.atD))).toFixed(2));
+    this.tp = Number((0.25 * ((3 * this.taLD) + Number(this.atD))));
     this.calculateTemparatureOfProductInKelvin();
    }
    public calculateTemparatureOfProductInKelvin(){
@@ -142,6 +155,15 @@ export class AppComponent implements OnInit{
    }
    public calculateEnthalpyOfVaporization(){
     this.lt = Math.round(Number(this.rg * this.vtc*this.tb2)*(Math.log(this.pc/Math.pow(10,5)))*(Math.pow((this.vtc-this.tpt),0.38))/(Math.pow((this.vtc-this.tb2),1.38)));
+   }
+   public calculateVolumeOfAirRequiredToRemoveMositure(){
+    this.va = Number(((this.mw * this.lt * this.ra * this.atK)/(this.cpa * this.pa*(this.to - this.taLK))));
+   }
+   public calculateVolumeFlowRate(){
+     this.v = Number((this.va / this.t));
+   }
+   public calculateMassFlowRate(){
+     this.mfr = Number((this.v * this.p));
    }
 
 
